16464affd9ae18f4b3f57c4f37f81aa407ddec64
nico233 <1601550204@qq.com>
2024-01-17 04:46:23 +0800

    feature: 添加 DELETE 方式请求方法

--- app/src/main/java/com/example/myapp/HttpConnect.java
+++ app/src/main/java/com/example/myapp/HttpConnect.java
@@ -7,8 +7,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class HttpConnect {
-    public static String BASE_URL= "http://locahost:8080";
-    private static String TAG = "http";
 
     public static String getHttpResult(String urlStr) {
         try {
@@ -17,15 +15,48 @@ public class HttpConnect {
             InputStream input = connect.getInputStream();
             BufferedReader in = new BufferedReader(new InputStreamReader(input));
             String line = null;
-            System.out.println(connect.getResponseCode());
             StringBuffer sb = new StringBuffer();
             while ((line = in.readLine()) != null) {
                 sb.append(line);
             }
             return sb.toString();
         } catch (Exception e) {
-            System.out.println(e.toString());
             return null;
         }
     }
+
+    public static String deleteHttpRequest(String urlStr) {
+        try {
+            URL url = new URL(urlStr);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("DELETE");
+            connection.setConnectTimeout(5000); // 设置连接超时时间，单位毫秒
+            connection.setReadTimeout(5000);    // 设置读取超时时间，单位毫秒
+
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                // 请求成功，读取服务器响应
+                InputStream input = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                String line;
+                StringBuffer response = new StringBuffer();
+
+                while ((line = reader.readLine()) != null) {
+                    response.append(line);
+                }
+
+                reader.close();
+                return response.toString();
+            } else {
+                // 请求失败，处理错误信息
+                System.out.println("Error Response Code: " + responseCode);
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
